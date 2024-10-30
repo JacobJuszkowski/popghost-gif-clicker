99ade735df992836af70e2c625e6c8b28ab52188
JacobJuszkowski <jjuszkowski@outlook.com>
2024-10-29 20:14:39 -0400

    Global counter api

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/get-global-counter', (req, res) => {
+    res.json({ globalCount: globalCounter });
+});
+
 wss.on('connection', (ws) => {
     console.log('New client connected');
     // Send the current global count immediately upon connection
